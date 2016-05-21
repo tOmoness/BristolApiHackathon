68348a3940eccf609b2b7a8307af80fb4138a577
Tom Harts <thomas.harts@gmail.com>
2016-05-21 13:37:18 +0100

    Start models project. Moar to follow!!!

---
+++ BristolApiHackathon.Models/ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BristolApiHackathon.Models
+{
+    public class ApiResponse
+    {
+        public bool? Success { get; set; }
+        public string LocalizedErrorMessage { get; set; }
+        public IList<object> Data { get; set; }
+    }
+}
---
+++ BristolApiHackathon.Models/Journey.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BristolApiHackathon.Models
+{
+    public class Journey
+    {
+        public string ArrivalTime { get; set; }
+        public string DepartureTime { get; set; }
+        public string SummaryHtml { get; set; }
+        public int? Duration { get; set; }
+        public IList<TransitJourneyLeg> Legs { get; set; }
+        public string OriginPlacePointId { get; set; }
+        public string DestinationPlacePointId { get; set; }
+    }
+}
