5ca884ad01ec8e89357e7bbca8029e05c92e2b3a
Russell Day <russell.day.coder@gmail.com>
2016-05-21 12:56:00 +0100

    Initial api class

---
+++ BristolApiHackathon.ApiClient/BristolApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BristolApiHackathon.ApiClient
+{
+    public class BristolApi
+    {
+        private string ApiUrl => $"https://bristol.api.urbanthings.io/api/{ApiVersion}/";
+        private const string ApiVersion = "2.0";
+        private readonly string _apiKey;
+
+        public BristolApi(string apiKey)
+        {
+            if (apiKey == null) throw new ArgumentNullException(nameof(apiKey));
+            _apiKey = apiKey;
+        }
+    }
+}
