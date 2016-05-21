076c7dea1ee9b9fcf865300fd3c7254b55df8773
Russell Day <russell.day.coder@gmail.com>
2016-05-21 13:10:03 +0100

    CreateDirectionsRequest + Client.Send

--- BristolApiHackathon.ApiClient/BristolApi.cs
+++ BristolApiHackathon.ApiClient/BristolApi.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BristolApiHackathon.ApiClient
 {
@@ -11,11 +13,19 @@ namespace BristolApiHackathon.ApiClient
         private string ApiUrl => $"https://bristol.api.urbanthings.io/api/{ApiVersion}/";
         private const string ApiVersion = "2.0";
         private readonly string _apiKey;
+        private readonly IRestClient _client;
 
         public BristolApi(string apiKey)
         {
             if (apiKey == null) throw new ArgumentNullException(nameof(apiKey));
             _apiKey = apiKey;
+
+            _client = BuildClient();
         }
+
+        private IRestClient BuildClient() => new RestClient(ApiUrl);
+
+        public ApiResponse Send(IRestRequest request)
+            => _client.Execute<ApiResponse>(request);
     }
 }
---
+++ BristolApiHackathon.ApiClient/BristolApiRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using RestSharp;
+
+namespace BristolApiHackathon.ApiClient
+{
+    public class BristolApiRequest : RestRequest
+    {
+        private BristolApiRequest(string resource, Method method) : base(resource, method)
+        {
+        }
+
+        public static BristolApiRequest CreateDirectionsRequest(DirectionsRequest directionsRequest)
+            => new BristolApiRequest("/plan/directions", Method.POST).AddBody(directionsRequest);
+    }
+}
